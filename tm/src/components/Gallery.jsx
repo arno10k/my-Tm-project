b84d54b2f31e804dfd358bcd6e5e7132d2915205
arno10k <arnoshkaminasyan@gmail.com>
2026-04-10 17:18:14 +0200

    Updated the gallery.jsx from a static to a dynamic

--- tm/src/components/Gallery.jsx
+++ tm/src/components/Gallery.jsx
@@ -1,38 +1,60 @@
-import messi from '../assets/messi.png'
-import cr7 from '../assets/cr7.jpg'
-import neymar from '../assets/neymar.png'
-import CL from '../assets/CL.png'
-import WC from '../assets/WC.webp'
-
+import React, { useState } from 'react'
+import getPhotoUrl from 'get-photo-url'
 
 const Gallery = () => {
-    return (
-      <> 
-       <input type="file" name='photo' id = ' addPhotoInput' />
-       <label htmlFor="addPhotoInput">
-        <i clasName='add-photo-button fas fa-plus-square'></i>
-       </label> 
 
-       <section className='gallery'>
-         <div className="item">
-            <img src={messi} className='item-image' alt="" />
-            <button className='delete-button'>Delete</button>
-         </div>
-          <div className="item">
-            <img src={cr7} className='item-image' alt="" />
-            <button className='delete-button'>Delete</button>
-         </div> <div className="item">
-            <img src={neymar} className='item-image' alt="" />
-            <button className='delete-button'>Delete</button>
-         </div> <div className="item">
-            <img src={CL} className='item-image' alt="" />
-            <button className='delete-button'>Delete</button>
-         </div> <div className="item">
-            <img src={WC} className='item-image' alt="" />
-            <button className='delete-button'>Delete</button>
-         </div>
-       </section>
-      </>
-    )
- } 
+  const [allPhotos, setAllPhotos] = useState([])
+
+  const addPhoto = async () => {
+
+    const newPhoto = {
+      id: Date.now(),
+      url: await getPhotoUrl('#addPhotoInput')
+    }
+
+    setAllPhotos([newPhoto, ...allPhotos])
+  }
+
+  return (
+    <>
+
+      <input
+        type="file"
+        name="photo"
+        id="addPhotoInput"
+        onChange={addPhoto}
+      />
+
+      <label htmlFor="addPhotoInput">
+
+        <i className="add-photo-button fas fa-plus-square"></i>
+
+      </label>
+
+      <section className="gallery">
+
+        {allPhotos.map((photo) => (
+
+          <div className="item" key={photo.id}>
+
+            <img
+              src={photo.url}
+              className="item-image"
+              alt=""
+            />
+
+            <button className="delete-button">
+              Delete
+            </button>
+
+          </div>
+
+        ))}
+
+      </section>
+
+    </>
+  )
+}
+
 export default Gallery
